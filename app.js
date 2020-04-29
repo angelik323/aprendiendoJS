c6e9b16b300de8381491f3aecd6dddb96b196f0e
AngelicaLoaiza <angelica.loaiza323@gmail.com>
2020-04-28 22:39:40 -0500

    se agrega variables con funcion en app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-document.getElementById('app').innerHTML = 'Hola Mundo';
+let nombre = prompt('Cual es tu nombre?');
+let edad = prompt('Cual es tu edad?');
+
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} 
+de ${edad} años`;
